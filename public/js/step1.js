a17b339ea11b5ed02d5dbed947ea9f47a8dff6d7
DTA32 <97021335+DTA32@users.noreply.github.com>
2023-09-16 16:59:24 +0700

    step1.js refactoring done by phpstorm suggestion

--- public/js/step1.js
+++ public/js/step1.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
-    const dataCount = $("#cardGroup").children().length;
-    var maskapaiArray = [];
-    var hargaArray = [];
-    var durasiArray = [];
+    // jquery selector vars
+    const cardGroup = $("#cardGroup");
+    const statusText = $("#statusText");
+    const durasiSlider = $("#durasiSlider");
+    const hargaSlider = $("#hargaSlider");
+    // init
+    const dataCount = cardGroup.children().length;
+    let maskapaiArray = [];
+    let hargaArray = [];
+    let durasiArray = [];
     for(let i = 0; i < dataCount; i++){
         const maskapai = $("#"+i+"_maskapai").text();
         if(!maskapaiArray.includes(maskapai)){
@@ -14,10 +20,10 @@ $(document).ready(function(){
         const durasi = parseInt($("#"+i+"_durasi").data('value'));
         durasiArray.push(durasi);
     }
-    var minDurasi = Math.min(...durasiArray);
-    var maxDurasi = Math.max(...durasiArray);
-    var minHarga = Math.min(...hargaArray);
-    var maxHarga = Math.max(...hargaArray);
+    const minDurasi = Math.min(...durasiArray);
+    const maxDurasi = Math.max(...durasiArray);
+    const minHarga = Math.min(...hargaArray);
+    const maxHarga = Math.max(...hargaArray);
     // function
     // sort
     $("#sortButton").click(function(){
@@ -25,70 +31,91 @@ $(document).ready(function(){
         $("#sortMenu").slideToggle();
     })
     $("#sortSubmit").click(function(){
-        const sortValue = $("input[name='sortRadio']:checked").val();
-        const dataCount = $("#cardGroup").children().length;
+        const sortValue = parseInt($("input[name='sortRadio']:checked").val());
+        console.log(sortValue);
+        const dataCount = cardGroup.children().length;
         resetOrder();
-        if(sortValue == 1){
-            for(let i = 0; i < dataCount; i++){
-                for(let j = 0; j < dataCount; j++){
-                    const harga1 = parseInt($("#"+j+"_harga").data('value'));
-                    const harga2 = parseInt($("#"+(j+1)+"_harga").data('value'));
-                    if(harga1 > harga2){
-                        $("#card_"+j).before($("#card_"+(j+1)));
+        if(isNaN(sortValue)){
+            statusText.text("Please select sort type");
+            statusText.removeClass("text-success");
+            statusText.addClass("text-danger");
+            statusText.show();
+            setTimeout(function(){
+                statusText.hide();
+            }, 2000);
+        }
+        else{
+            if(sortValue === 1){
+                for(let i = 0; i < dataCount; i++){
+                    for(let j = 0; j < dataCount; j++){
+                        const harga1 = parseInt($("#"+j+"_harga").data('value'));
+                        const harga2 = parseInt($("#"+(j+1)+"_harga").data('value'));
+                        if(harga1 > harga2){
+                            $("#card_"+j).before($("#card_"+(j+1)));
 
+                        }
                     }
                 }
             }
-        }
-        else if(sortValue == 2){
-            for(let i = 0; i < dataCount; i++){
-                for(let j = 0; j < dataCount; j++){
-                    const durasi1 = parseInt($("#"+j+"_durasi").data('value'));
-                    const durasi2 = parseInt($("#"+(j+1)+"_durasi").data('value'));
-                    if(durasi1 > durasi2){
-                        $("#card_"+j).before($("#card_"+(j+1)));
+            else if(sortValue === 2){
+                for(let i = 0; i < dataCount; i++){
+                    for(let j = 0; j < dataCount; j++){
+                        const durasi1 = parseInt($("#"+j+"_durasi").data('value'));
+                        const durasi2 = parseInt($("#"+(j+1)+"_durasi").data('value'));
+                        if(durasi1 > durasi2){
+                            $("#card_"+j).before($("#card_"+(j+1)));
+                        }
                     }
                 }
             }
-        }
-        else if(sortValue == 3){
-            for(let i = 0; i < dataCount; i++){
-                for(let j = 0; j < dataCount; j++){
-                    const keberangkatan1 = parseInt($("#"+j+"_keberangkatan").data('value'));
-                    const keberangkatan2 = parseInt($("#"+(j+1)+"_keberangkatan").data('value'));
-                    if(keberangkatan1 > keberangkatan2){
-                        $("#card_"+j).before($("#card_"+(j+1)));
+            else if(sortValue === 3){
+                for(let i = 0; i < dataCount; i++){
+                    for(let j = 0; j < dataCount; j++){
+                        const keberangkatan1 = parseInt($("#"+j+"_keberangkatan").data('value'));
+                        const keberangkatan2 = parseInt($("#"+(j+1)+"_keberangkatan").data('value'));
+                        if(keberangkatan1 > keberangkatan2){
+                            $("#card_"+j).before($("#card_"+(j+1)));
+                        }
                     }
                 }
             }
-        }
-        else if(sortValue == 4){
-            for(let i = 0; i < dataCount; i++){
-                for(let j = 0; j < dataCount; j++){
-                    const kedatangan1 = parseInt($("#"+j+"_kedatangan").data('value'));
-                    const kedatangan2 = parseInt($("#"+(j+1)+"_kedatangan").data('value'));
-                    if(kedatangan1 > kedatangan2){
-                        $("#card_"+j).before($("#card_"+(j+1)));
+            else if(sortValue === 4){
+                for(let i = 0; i < dataCount; i++){
+                    for(let j = 0; j < dataCount; j++){
+                        const kedatangan1 = parseInt($("#"+j+"_kedatangan").data('value'));
+                        const kedatangan2 = parseInt($("#"+(j+1)+"_kedatangan").data('value'));
+                        if(kedatangan1 > kedatangan2){
+                            $("#card_"+j).before($("#card_"+(j+1)));
+                        }
                     }
                 }
             }
+            sortSuccess();
         }
-        sortSuccess();
     })
     $("#sortReset").click(function(){
         resetOrder();
         $('input[name="sortRadio"]').prop('checked', false);
+        statusText.text("Sort cleared");
+        statusText.removeClass("text-danger");
+        statusText.addClass("text-success");
+        statusText.show();
+        setTimeout(function(){
+            statusText.hide();
+        }, 2000);
     });
     function resetOrder() {
-        for(let i = 0; i < $("#cardGroup").children().length; i++){
-            $("#cardGroup").append($("#card_"+i));
+        for(let i = 0; i < cardGroup.children().length; i++){
+            cardGroup.append($("#card_"+i));
         }
     }
     function sortSuccess(){
-        $("#statusText").text("Sorted successfully");
-        $("#statusText").show();
+        statusText.text("Sorted successfully");
+        statusText.removeClass("text-danger");
+        statusText.addClass("text-success");
+        statusText.show();
         setTimeout(function(){
-            $("#statusText").hide();
+            statusText.hide();
         }, 2000);
     }
     // filter
@@ -110,10 +137,10 @@ $(document).ready(function(){
         return `${hours}j ${minutes}m`;
     }
     $("#durasiFilterButton").click(function(){
-        $("#durasiRange").text(toHoursAndMinutes($("#durasiSlider").slider("values", 0))+" - "+ toHoursAndMinutes($("#durasiSlider").slider("values", 1)));
+        $("#durasiRange").text(toHoursAndMinutes(durasiSlider.slider("values", 0))+" - "+ toHoursAndMinutes(durasiSlider.slider("values", 1)));
         $("#durasiRangeGroup").slideToggle();
     })
-    $("#durasiSlider").slider({
+    durasiSlider.slider({
         range: true,
         min: minDurasi,
         max: maxDurasi,
@@ -128,10 +155,10 @@ $(document).ready(function(){
         return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(number);
     }
     $("#hargaFilterButton").click(function(){
-        $("#hargaRange").text(rupiah($("#hargaSlider").slider("values", 0))+" - "+ rupiah($("#hargaSlider").slider("values", 1)));
+        $("#hargaRange").text(rupiah(hargaSlider.slider("values", 0))+" - "+ rupiah(hargaSlider.slider("values", 1)));
         $("#hargaRangeGroup").slideToggle();
     })
-    $("#hargaSlider").slider({
+    hargaSlider.slider({
         range: true,
         min: minHarga,
         max: maxHarga,
@@ -142,6 +169,7 @@ $(document).ready(function(){
         }
     });
     $("#filterSubmit").click(function(){
+        // check checked filter
         const maskapaiFilter = [];
         const keberangkatanFilter = [];
         for(let i = 0; i < maskapaiArray.length; i++){
@@ -155,42 +183,32 @@ $(document).ready(function(){
                 keberangkatanFilter.push(range);
             }
         }
-        const keberangkatanFilterEmpty = keberangkatanFilter.length == 0;
-        const maskapaiFilterEmpty = maskapaiFilter.length == 0;
-        const durasiFilterEmpty = $("#durasiSlider").slider("values", 0) == minDurasi && $("#durasiSlider").slider("values", 1) == maxDurasi;
-        const hargaFilterEmpty = $("#hargaSlider").slider("values", 0) == minHarga && $("#hargaSlider").slider("values", 1) == maxHarga;
+        // check if all filter empty
+        const keberangkatanFilterEmpty = parseInt(keberangkatanFilter.length) === 0;
+        const maskapaiFilterEmpty = parseInt(maskapaiFilter.length) === 0;
+        const durasiFilterEmpty = parseInt(durasiSlider.slider("values", 0)) === minDurasi && parseInt(durasiSlider.slider("values", 1)) === maxDurasi;
+        const hargaFilterEmpty = parseInt(hargaSlider.slider("values", 0)) === minHarga && parseInt(hargaSlider.slider("values", 1)) === maxHarga;
         if(keberangkatanFilterEmpty && maskapaiFilterEmpty && durasiFilterEmpty && hargaFilterEmpty){
             $("#filterReset").trigger("click");
+            statusText.text("No filter selected, filter cleared");
+            statusText.removeClass("text-success");
+            statusText.addClass("text-danger");
+            statusText.show();
+            setTimeout(function(){
+                statusText.hide();
+            }, 2000);
         }
+        // filter
         else{
             for(let i = 0; i < dataCount; i++){
                 const maskapai = $("#"+i+"_maskapai").text();
                 const keberangkatan = parseInt($("#"+i+"_keberangkatan").data('value'));
                 const durasi = parseInt($("#"+i+"_durasi").data('value'));
                 const harga = parseInt($("#"+i+"_harga").data('value'));
-                // if(maskapaiFilter.length > 0){
-                //     if(!maskapaiFilter.includes(maskapai)){
-                //         $("#card_"+i).hide();
-                //     }
-                //     else{
-                //         $("#card_"+i).show();
-                //     }
-                // }
-                // if(keberangkatanFilter.length > 0){
-                //     for(let j = 0; j < keberangkatanFilter.length; j++){
-                //         if(keberangkatan >= keberangkatanFilter[j][0] && keberangkatan <= keberangkatanFilter[j][1]){
-                //             $("#card_"+i).show();
-                //             break;
-                //         }
-                //         else{
-                //             $("#card_"+i).hide();
-                //         }
-                //     }
-                // }
                 const maskapaiFilterCondition = maskapaiFilter.length > 0 ? maskapaiFilter.includes(maskapai) : true;
                 const keberangkatanFilterCondition = keberangkatanFilter.length > 0 ? keberangkatanFilter.some(range => keberangkatan >= range[0] && keberangkatan <= range[1]) : true;
-                const durasiFilterCondition = (durasi >= $("#durasiSlider").slider("values", 0)) && (durasi <= $("#durasiSlider").slider("values", 1));
-                const hargaFilterCondition = harga >= $("#hargaSlider").slider("values", 0) && harga <= $("#hargaSlider").slider("values", 1);
+                const durasiFilterCondition = (durasi >= durasiSlider.slider("values", 0)) && (durasi <= durasiSlider.slider("values", 1));
+                const hargaFilterCondition = harga >= hargaSlider.slider("values", 0) && harga <= hargaSlider.slider("values", 1);
                 if(maskapaiFilterCondition && keberangkatanFilterCondition && durasiFilterCondition && hargaFilterCondition){
                     $("#card_"+i).show();
                 }
@@ -198,8 +216,8 @@ $(document).ready(function(){
                     $("#card_"+i).hide();
                 }
             }
+            filterSuccess();
         }
-        filterSuccess();
     })
     $("#filterReset").click(function(){
         for(let i = 0; i < maskapaiArray.length; i++){
@@ -208,19 +226,28 @@ $(document).ready(function(){
         for(let i = 0; i < 4; i++){
             $("#keberangkatanCheck_"+i).prop('checked', false);
         }
-        $("#durasiSlider").slider("values", [minDurasi, maxDurasi]);
+        durasiSlider.slider("values", [minDurasi, maxDurasi]);
         $("#durasiRange").text(toHoursAndMinutes(minDurasi)+" - "+ toHoursAndMinutes(maxDurasi));
-        $("#hargaSlider").slider("values", [minHarga, maxHarga]);
+        hargaSlider.slider("values", [minHarga, maxHarga]);
         $("#hargaRange").text(rupiah(minHarga)+" - "+ rupiah(maxHarga));
         for(let i = 0; i < dataCount; i++){
             $("#card_"+i).show();
         }
+        statusText.text("Filter cleared");
+        statusText.removeClass("text-danger");
+        statusText.addClass("text-success");
+        statusText.show();
+        setTimeout(function(){
+            statusText.hide();
+        }, 2000);
     })
     function filterSuccess(){
-        $("#statusText").text("Filtered successfully");
-        $("#statusText").show();
+        statusText.text("Filtered successfully");
+        statusText.removeClass("text-danger");
+        statusText.addClass("text-success");
+        statusText.show();
         setTimeout(function(){
-            $("#statusText").hide();
+            statusText.hide();
         }, 2000);
     }
 })
